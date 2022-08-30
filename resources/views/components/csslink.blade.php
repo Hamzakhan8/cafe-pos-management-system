ba05f42fc024d920d975737d24851e33ca0c1754
zalmi <78726819+zalmicoding@users.noreply.github.com>
2022-08-30 04:20:31 -0700

    dashboard view set

---
+++ resources/views/components/csslink.blade.php
@@ -0,0 +1,6 @@
+<link rel="icon" type="image/png" sizes="16x16" href="{{asset('images/favicon.png')}}">
+<link href="{{asset('vendor/jqvmap/css/jqvmap.min.css')}}" rel="stylesheet">
+<link rel="stylesheet" href="{{asset('vendor/chartist/css/chartist.min.css')}}">
+<link href="{{asset('vendor/bootstrap-select/dist/css/bootstrap-select.min.css')}}" rel="stylesheet">
+<link href="{{asset('css/style.css')}}" rel="stylesheet">
+<link href="https://cdn.lineicons.com/2.0/LineIcons.css" rel="stylesheet">
