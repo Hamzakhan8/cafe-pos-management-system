e5d5175f0a264dbf354aca63f81c461a552f96eb
zalmi <78726819+zalmicoding@users.noreply.github.com>
2022-08-30 04:31:12 -0700

    dashboard set

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.master')
+@section('title','dashboard')
+@section('content')
+
+<div class="content-body">
+    <!-- row -->
+    <div class="container-fluid">
+        <div class="form-head d-flex mb-3 align-items-start">
+            <div class="mr-auto d-none d-lg-block">
+                <h2 class="text-black font-w600 mb-0">Dashboard</h2>
+                <p class="mb-0">Welcome to queta Cafe !</p>
+            </div>
+            
+        </div> 
+            <div class="form-group">
+              <label for="">email</label>
+              <input type="email" class="form-control" name="" id="" aria-describedby="emailHelpId" placeholder="">
+              <small id="emailHelpId" class="form-text text-muted">Help text</small>
+            </div>    
+            <div class="form-group">
+                <label for="">email</label>
+                <input type="email" class="form-control" name="" id="" aria-describedby="emailHelpId" placeholder="">
+                <small id="emailHelpId" class="form-text text-muted">Help text</small>
+              </div> <div class="form-group">
+                <label for="">email</label>
+                <input type="email" class="form-control" name="" id="" aria-describedby="emailHelpId" placeholder="">
+                <small id="emailHelpId" class="form-text text-muted">Help text</small>
+              </div> <div class="form-group">
+                <label for="">email</label>
+                <input type="email" class="form-control" name="" id="" aria-describedby="emailHelpId" placeholder="">
+                <small id="emailHelpId" class="form-text text-muted">Help text</small>
+              </div> <div class="form-group">
+                <label for="">email</label>
+                <input type="email" class="form-control" name="" id="" aria-describedby="emailHelpId" placeholder="">
+                <small id="emailHelpId" class="form-text text-muted">Help text</small>
+              </div> <div class="form-group">
+                <label for="">email</label>
+                <input type="email" class="form-control" name="" id="" aria-describedby="emailHelpId" placeholder="">
+                <small id="emailHelpId" class="form-text text-muted">Help text</small>
+              </div>    
+         
+    </div>
+</div>
